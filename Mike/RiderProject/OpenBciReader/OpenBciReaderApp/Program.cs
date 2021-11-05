0aaf35c0aad403ea8f6b53be5d618d51024542e7
Michael Elia <michaeljelia@gmail.com>
2021-11-05 15:57:48 -0400

    New C# Console Project

---
+++ Mike/RiderProject/OpenBciReader/OpenBciReaderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenBciReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
