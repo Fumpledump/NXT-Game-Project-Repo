2f13fa367bf579bee576d395a0a2c1874b2e75b0
Michael Elia <michaeljelia@gmail.com>
2021-11-05 16:32:30 -0400

    Added VS Project

---
+++ Mike/VsProject/OpenBciReaderProject/OpenBciReaderProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenBciReaderProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
